8d3e4845b783503ec96c41422bf657081b0857ff
akshayaasa81-del <akshayaasa81@gmail.com>
2025-12-15 16:29:13 +0530

    Add files via upload

---
+++ ShoppingBill.java
@@ -0,0 +1,20 @@
+package Shopping;
+
+import java.util.Scanner;
+
+public class ShoppingBill {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System);
+		System.out.println("Enter Product Price");
+		double price = sc.nectDouble();
+		System.out.println("Enter Quality:");
+		int qty = sc.nextInt();
+		double total = price * qty;
+		double discount = total * 0.10;
+		double finalAmount = total - discount;
+		System.out.println("Total Amount:" + total);
+		System.out.println("Discount:" + discount);
+		System.out.println("Final Payable Amount:" +  finalAmount);
+	}
+
+}
